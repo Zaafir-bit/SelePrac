ec84be8c32277b34484035811f85d7b39a3524bd
KDS <KDS@192.168.10.13>
2020-09-09 16:07:48 +0500

    Saleshub

--- installation/src/installation/SalesHub.java
+++ installation/src/installation/SalesHub.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.*;
+
 
 
 public class SalesHub {
